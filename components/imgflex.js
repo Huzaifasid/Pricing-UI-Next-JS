4dbd8042feb4b89ba39910abe6cd201c742894af
huzaifasiddiqui <siddiquihuzaifa68@gmail.com>
2023-01-30 22:23:56 +0500

    Pricing UI with Nextjs

--- components/imgflex.js
+++ components/imgflex.js
@@ -2,14 +2,12 @@ import { Flex, Image, Square, Text } from "@chakra-ui/react";
 import React from "react";
 
 const ImgFlex = ({ img, text }) => {
-  console.log(img);
   return (
     <>
-      <Flex className="items-center md:m-auto mt-3 ml-3  ">
+      <Flex className="items-center  mt-3 ml-3 md:px-10  ">
         <Square>
           <Image
             borderRadius="full"
-            // boxSize="20px"
             className="w-6 h-6 md:w-11 md:h-11 "
             src={img}
             alt="icon"
@@ -17,7 +15,7 @@ const ImgFlex = ({ img, text }) => {
         </Square>
         <Square className=" ml-3 md:ml-4   ">
           <Text
-            className=" w-[240px] md:w-[220px] text-[12px] md:text-[17px]"
+            className=" w-[240px] md:w-[210px] text-[12px] md:text-[16px]"
             fontWeight={800}
           >
             {text}
